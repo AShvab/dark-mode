eab0142403e7776a254f072ee223b1aca09aa35e
AShvab <115644652+AShvab@users.noreply.github.com>
2023-04-09 11:28:39 +0000

    Deploying to gh-pages from @ AShvab/dark-mode@4808b036091640efed7ecb4355b1df12b9d122f0 🚀

--- index.4587a9f2.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".toggle-mode"),t=document.querySelector('.changeTheme[data-theme="light"]'),l=document.querySelector('.changeTheme[data-theme="dark"]');e.addEventListener("click",(e=>{e.preventDefault(),document.body.classList.toggle("dark-mode"),t.style.display="none"===t.style.display?"block":"none",l.style.display="block"===l.style.display?"none":"block"}));
-//# sourceMappingURL=index.4587a9f2.js.map
--- index.aee43940.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".toggle-mode"),t=document.querySelector('.changeTheme[data-theme="light"]'),l=document.querySelector('.changeTheme[data-theme="dark"]');e.addEventListener("click",(function(e){e.preventDefault(),document.body.classList.toggle("dark-mode"),t.style.display="none"===t.style.display?"block":"none",l.style.display="block"===l.style.display?"none":"block"}))}();
-//# sourceMappingURL=index.aee43940.js.map
