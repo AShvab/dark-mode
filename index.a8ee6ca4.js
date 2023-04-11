67de4d6b5304d7f77257130e2916e1320cc206d5
AShvab <115644652+AShvab@users.noreply.github.com>
2023-04-11 19:57:09 +0000

    Deploying to gh-pages from @ AShvab/dark-mode@cc0c7d6cc9def4e35aa4d8f7e0def49a8cc77297 🚀

--- index.a8ee6ca4.js
+++
@@ -1,2 +0,0 @@
-const o=document.querySelector(".upward");o.addEventListener("click",(function(){window.scrollTo({top:0,behavior:"smooth"})})),window.addEventListener("scroll",(function(){o.classList.toggle("active",window.scrollY>500)}));
-//# sourceMappingURL=index.a8ee6ca4.js.map
--- index.b07e91f2.js
+++
@@ -1,2 +0,0 @@
-!function(){var o=document.querySelector(".upward");o.addEventListener("click",(function(){window.scrollTo({top:0,behavior:"smooth"})})),window.addEventListener("scroll",(function(){o.classList.toggle("active",window.scrollY>500)}))}();
-//# sourceMappingURL=index.b07e91f2.js.map
