cc0c7d6cc9def4e35aa4d8f7e0def49a8cc77297
AShvab <shvabovska@gmail.com>
2023-04-11 22:56:47 +0300

    changed button position

--- src/js/upward.js
+++ src/js/upward.js
@@ -2,8 +2,11 @@
 const scrollEl = document.querySelector('.upward');
 scrollEl.addEventListener('click', scrollTopTop);
 
-window.addEventListener('scroll', function () {    
-    scrollEl.classList.toggle('active', window.scrollY > 500);
+
+//кнопка буде показуватися, коли користувач прокручує вниз на більше половини висоти вікна браузера
+window.addEventListener('scroll', function () {
+    const windowHeight = window.innerHeight;
+    scrollEl.classList.toggle('active', window.scrollY > windowHeight * 0.5);
 });
 
 function scrollTopTop() {
